0742c7d31233658ee1c4c1cda099ea2f6a779a28
veys66 <veysdoganer@gmail.com>
2023-04-08 22:29:00 +0300

    utilities

--- src/main/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/main/java/com/cydeo/utilities/SpartanTestBase.java
@@ -9,7 +9,7 @@ public abstract class SpartanTestBase {
 
     @BeforeAll
     public static void init(){
-        //save baseurl inside this variable so that we dont need to type each http method.
+        //save baseurl inside this variable so that we don't need to type each http method.
         baseURI = "http://3.216.30.92:8000";
 
         String dbUrl = "jdbc:oracle:thin:@3.216.30.92:1521:xe";
